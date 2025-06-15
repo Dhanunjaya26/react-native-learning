43b84be3e4cdd3d014bb1727257466830722d6d3
Dhanunjaya26 <dhanunjayaputluru26@gmail.com>
2025-06-15 17:41:38 +0530

    splitting app into components for reusability

--- App.js
+++ App.js
@@ -1,14 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import {
-  Button,
-  FlatList,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
+import { Button, FlatList, StyleSheet, TextInput, View } from "react-native";
+
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 const GOALS_LIST = [
   {
@@ -18,7 +13,7 @@ const GOALS_LIST = [
   },
   {
     id: 1,
-    title: "Master DSA",
+    title: "Master DSAA",
     isComplete: false,
   },
 ];
@@ -39,15 +34,11 @@ export default function App() {
   };
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Enter a goal"
-          value={inputValue}
-          onChangeText={setInputValue}
-        />
-        <Button title="Add Goal" onPress={addGoalHandler} />
-      </View>
+      <GoalInput
+        inputValue={inputValue}
+        addGoalHandler={addGoalHandler}
+        setInputValue={setInputValue}
+      />
       <View style={styles.goalsContainer}>
         <FlatList
           alwaysBounceVertical={false}
@@ -55,13 +46,10 @@ export default function App() {
           renderItem={(itemData) => {
             // you don't have to give key prop to the elements in the list. FlatList takes care of it if you provide key property in each object of goals array.
             // FlatList calls renderItem({ item, index, separators }). So, the argument in callback function assigned to renderItem(itemData) is actually an object. So, inorder to extract title from the goal, you have to do itemData.item.title
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.title}</Text>
-              </View>
-            );
+            return <GoalItem goalText={itemData.item.title} />;
           }}
           keyExtractor={(item, index) => {
+            // If you don't provide name as key in the item property, then we should extract it in the name of key
             return item.id;
           }}
         />
@@ -76,32 +64,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 25,
     flex: 1,
   },
-  inputContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 8,
-    padding: 8,
-  },
   goalsContainer: {
     flex: 5,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
-  },
-  goalText: {
-    color: "white",
   },
 });
